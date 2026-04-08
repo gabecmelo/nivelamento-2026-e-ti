f2724ab001503902045d33c8bc96e283f04ac1af
gabriel-v15-laptop <lokomemog@gmail.com>
2026-04-08 20:14:19 -0300

    chore: main and prisma config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  const PORT = process.env.PORT ?? 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('API Nivelamento Veiculos')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  app.enableCors({ origin: 'http://localhost:4200' });
+
+  await app.listen(3000);
+  console.log(`API rodando em http://localhost:$´PORT}`);
+
 }
 bootstrap();
---
+++ backend/src/prisma/prisma.service.ts
@@ -0,0 +1,17 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from 'generated/prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  async onModuleInit() {
+    await this.$connect;
+    console.log('Conectado com o banco');
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect;
+  }
+}
