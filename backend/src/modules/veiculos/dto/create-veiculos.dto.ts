94be07305eb42fb2cdee9ada8aec502f013de338
gabriel-v15-laptop <lokomemog@gmail.com>
2026-04-08 21:42:15 -0300

    feat: create veiculos dto

---
+++ backend/src/modules/veiculos/dto/create-veiculos.dto.ts
@@ -0,0 +1,17 @@
+import { IsString, IsInt, Min } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateVeiculoDto {
+  @ApiProperty({ example: 'Fiat Palio' })
+  @IsString()
+  modelo!: string;
+
+  @ApiProperty({ example: 1998 })
+  @IsInt()
+  @Min(1900)
+  anoFabricacao!: number;
+
+  @ApiProperty({ example: 'AUG8L93' })
+  @IsInt()
+  placa!: string;
+}
